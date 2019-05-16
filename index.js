9cf300831dc6a52d122b2fc715226c767a7fe92b
Elaine Thai <ethai1827@gmail.com>
2019-05-16 00:26:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function getFirstSelector(selector) {
 }
 
 function nestedTarget() {
-  return document.getElementById('nested').getElementsByClassName('target');
+  return document.getElementById('nested').getElementsByClassName('target')[0];
 }
 
 function increaseRankBy(n) {
