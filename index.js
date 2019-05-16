00dd3e7d9a556af92dbb9ed8a3b5a6176d6c78b2
Elaine Thai <ethai1827@gmail.com>
2019-05-16 00:26:27 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+function getFirstSelector(selector) {
+  return document.getElementById('app').querySelector(selector);
+}
 
+function nestedTarget() {
+  return document.getElementById('nested').getElementsByClassName('target')[0];
+}
+
+function increaseRankBy(n) {
+  var lis = document.getElementById('app').querySelectorAll('ul.ranked-list'); 
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = (parseInt(lis[i].innerHTML) + n).toString();
+  }
+}
+
+function deepestChild() {
+  var node = document.querySelector('div#grand-node');
+  while (node.children.length !== 0) {
+    node = node.children[0];
+  }
+  return node;
+}
